832fb1408107b685d2ce97f5829b687a37e46772
Erik Bakker <erik.bakker@lunatech.com>
2012-02-01 22:53:22 +0100

    Remove dependency on Netty and simplifications

--- src/play/modules/less/Plugin.java
+++ src/play/modules/less/Plugin.java
@@ -1,8 +1,5 @@
 package play.modules.less;
 
-import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.ETAG;
-import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.LAST_MODIFIED;
-
 import java.io.PrintStream;
 import java.util.Date;
 
@@ -36,7 +33,7 @@ public class Plugin extends PlayPlugin {
             }
             return true;
         }
-        return super.serveStatic(file, request, response);
+        return false;
     }
     
     private void handleResponse(VirtualFile file, Request request, Response response) {
@@ -55,16 +52,16 @@ public class Plugin extends PlayPlugin {
             response.status = Http.StatusCode.NOT_MODIFIED;
         }
         if (useEtag) {
-            response.setHeader(ETAG, etag);
+            response.setHeader("ETag", etag);
         }
     }
     
     private void handleOk(Request request, Response response, VirtualFile file, String etag, long lastModified) {
         response.status = 200;
         response.print(playLessEngine.get(file.getRealFile()));
-        response.setHeader(LAST_MODIFIED, Utils.getHttpDateFormatter().format(new Date(lastModified)));
+        response.setHeader("Last-Modified", Utils.getHttpDateFormatter().format(new Date(lastModified)));
         if (useEtag) {
-            response.setHeader(ETAG, etag);
+            response.setHeader("ETag", etag);
         }
     }
 }
